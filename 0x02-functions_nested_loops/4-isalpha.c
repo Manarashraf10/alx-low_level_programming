3f4db0b859998c362869c126db4b84d99fcb2150
Manarashraf10 <hamza13.7.18@gmail.com>
2023-04-26 08:32:24 -0700

    l

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * Description: A function checks if character
+ * is a letter both lowercase or uppercase
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true else 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+		if (c >= 65 && c <= 90)
+			return (1);
+	return (0);
+}
