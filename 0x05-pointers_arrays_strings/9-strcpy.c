2f48a30ed658ed8e801c9298bd89d5eed918d5d1
Manarashraf10 <hamza13.7.18@gmail.com>
2023-05-03 17:41:36 -0700

    l

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,9 +11,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	/*copy src into dest*/
-	strcpy(dest, src);
-	printf("strcpy(dest, src): %s\n", dest);
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
 
 	return (dest);
 }
