afbe127db3b884795297975d3d0f4aafa0664e19
Manarashraf10 <hamza13.7.18@gmail.com>
2023-05-14 15:18:39 -0700

    l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguments passed
+ * @argc: the number of command line arguments
+ * @argv: pointer to an array containing the program command line arguments
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv[];
+	printf("%d/n", argc - 1);
+	return (0);
+}
