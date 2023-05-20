0adf4866cbd3639c30efff2a291c0df4ffdc7393
Manarashraf10 <hamza13.7.18@gmail.com>
2023-05-19 19:43:43 -0700

    l

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -31,12 +31,12 @@ void *m;
 
 if (size == 0 || nmemb == 0)
 return (NULL);
-m = malloc(nmemb * sizeof(int));
+m = malloc(sizeof(int) * nmemb);
 
 if (m == 0)
 return (NULL);
 
-_memset(m, 0, nmemb * sizeof(int));
+_memset(m, 0, sizeof(int) * nmemb);
 
 return (m);
 }
