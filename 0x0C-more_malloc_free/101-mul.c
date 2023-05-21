cafa188d204b4b72d9aa9abadce105960450ef55
Manarashraf10 <hamza13.7.18@gmail.com>
2023-05-21 16:56:47 -0700

    l

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -43,8 +43,8 @@ char *big_multiply(char *s1, char *s2)
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
 	r = malloc(a = x = l1 + l2);
-	if (r == 0)
-		printf("Error\n");
+	if (!r)
+		printf("Error\n"), exit(98);
 	while (a--)
 		r[a] = 0;
 
