2b9b18341ca439b123ffce777096325968dab824
Manarashraf10 <hamza13.7.18@gmail.com>
2023-05-25 17:11:42 -0700

    l

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,11 @@
+#ifndef CALC_H
+#define CALC_H
+
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * struct op - struct op
+ * struct op - Struct op
  *
  * @op: the operator
  * @f: the function associated
