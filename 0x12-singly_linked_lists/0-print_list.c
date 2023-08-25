df5c0952c5ffae0fea6fcbfaf66c3a0dcba25950
Manarashraf10 <hamza13.7.18@gmail.com>
2023-08-25 08:01:17 -0700

    l

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns the length of the string
+ * @s: the string to check the length
+ * Return: integer length of string
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - function prints all the elements of a linked list
+ * @h: pointer to start of the list
+ * Return: the size of the list
+*/
+
+size_t print_list(const list_t *h);
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
